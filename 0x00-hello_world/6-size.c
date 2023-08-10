b7f7ddee3cccf6a381d3311624c781c06268d89e
Alshimaa Mamdouh <alshimaa.mamdouh.abdelaziz@gmail.com>
2023-08-10 15:21:23 -0700

    excutable

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
-int main (void){
-
-printf("Size of char:%d Byte(s)\n",sizeof(char));
-printf("Size of int:%d Byte(s)\n",sizeof(int));
-printf("Size of long int:%d Byte(s)\n",sizeof(long int));
-printf("Size of long long int:%d Byte(s)\n",sizeof(long long int));
-printf("Size of float:%d Byte(s)\n",sizeof(float));
-return(0);
+/**
+ * main - program that print size of different sizes
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+printf("Size of a char: %d byte(s)\n", sizeof(char));
+printf("Size of an int: %d byte(s)\n", sizeof(int));
+printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+printf("Size of a float: %d byte(s)\n", sizeof(float));
+return (0);
 }
