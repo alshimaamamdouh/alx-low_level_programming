74f942b0b4916a1f74245ca654f85a0766a137eb
Alshimaa Mamdouh <alshimaa.mamdouh.abdelaziz@gmail.com>
2023-08-10 15:07:27 -0700

    excutable

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
- * main - program print line using puts function
- *Return : 0 in success
+ * main - program that print line using pritf function
+ * Return: 0 (Success)
  */
 int main(void)
 {
-
-puts ("\"Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
