110551c0b67240f1aedfb7c69a5927cdedca2608
Alshimaa Mamdouh <alshimaa.mamdouh.abdelaziz@gmail.com>
2023-08-10 14:44:25 -0700

    excutable

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
-int main(void){
+/*
+ * main: use puts to print aline
+ *Return : 0 in success
+ */
+int main(void)
+{
 
-puts("Programming is like building a multilingual puzzle \n");
+puts("\"Programming is like building a multilingual puzzle");
 return(0);
 }
