dd706bd048fd889d28a0581d3acb9d5a55f89a37
Alshimaa Mamdouh <alshimaa.mamdouh.abdelaziz@gmail.com>
2023-08-10 15:03:51 -0700

    excutable

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
  * main - program that print line using pritf function
- * return : 0 (success)
- * */
-int main (void)
+ * Return: 0 (Success)
+ */
+int main(void)
 {
-
-printf ("with proper grammar, but the outcome is a piece of art,\n");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
