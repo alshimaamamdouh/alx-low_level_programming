c852933a956691790ef235ea182a381391d810b9
Alshimaa Mamdouh <alshimaa.mamdouh.abdelaziz@gmail.com>
2023-08-15 18:06:52 -0700

    -times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * times_table - Prints the 9 times table, starting with 0.
+ */
+
+void times_table(void)
+{
+	int i;
+	int j;
+	int prod;
+		for (i = 0; i < 10; i++)
+		{
+			for (j = 0; j < 10; j++)
+			{
+				prod = i * j;
+
+				if (prod <= 9)
+				_putchar(' ');
+			else
+
+				_putchar((prod / 10) + '0');
+
+			_putchar((prod % 10) + '0');
+
+			_putchar(',');
+			_putchar(' ');
+			}
+			_putchar('\n');
+		}
+}
