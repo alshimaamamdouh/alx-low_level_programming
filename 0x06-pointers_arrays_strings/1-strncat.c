f17b7f9c059fe485820467b7808014ca2553279a
Alshimaa Mamdouh <alshimaa.mamdouh.abdelaziz@gmail.com>
2023-08-23 14:25:25 -0700

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -12,21 +12,19 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-int i = 0, j = 0;
+	int i = 0, j = 0;
 
-while (dest[i] != '\0')
-i++;
-
-while (src[j] != '\0' && j < n)
-{
-dest[i] = src[j];
-i++;
-j++;
-}
-if (j < n)
-{
-dest[j] = '\0';
-}
-
-return (dest);
+	while (dest[i] != '\0')
+		i++;
+	while (j < n && src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	if (j < n)
+	{
+	dest[i] = '\0';
+	}
+	return (dest);
 }
