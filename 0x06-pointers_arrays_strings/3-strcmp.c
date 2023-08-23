852ec982554602b485a57fb3cae2222c9500c946
Alshimaa Mamdouh <alshimaa.mamdouh.abdelaziz@gmail.com>
2023-08-23 15:27:38 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,22 +10,11 @@
 
 int _strcmp(char *s1, char *s2)
 {
-
-int flag = 0, i = 0;
-
-while (s1[i] != '\0' && s2[i] != '\0')
-{
-if (s1[i] != s2[i])
-
-{
-flag = 1;
-break;
-}
-i++;
-}
-if (flag == 0)
-return (0);
-
-else
-return (*s1 - *s2);
+	for (; (*s1 != '\0' && *s2 != '\0') && (*s1 == *s2); s1++, s2++)
+		;
+	if (*s1 == *s2)
+	{
+		return (0);
+	}
+	return (*s1 - *s2);
 }
