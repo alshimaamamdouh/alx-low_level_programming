9dcf777322b5ea7726d6a02992e7bd6d6bb25f7d
Alshimaa Mamdouh <alshimaa.mamdouh.abdelaziz@gmail.com>
2023-08-30 17:13:38 -0700

    2-strlen_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - Returns Length of String
+ * @s: input string
+ *
+ * Return: On success 
+ */
+int _strlen_recursion(char *s)
+{
+int i;
+if (*s != '\0')
+{	
+i = 1 +  _strlen_recursion(s + 1);
+return (i);
+}
+return (0);
+}
