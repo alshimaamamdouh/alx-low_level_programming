993f10affd6a38955bed15603bce6dc6e6ba34db
Alshimaa Mamdouh <alshimaa.mamdouh.abdelaziz@gmail.com>
2023-09-21 12:32:23 -0700

    error correction

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,7 +8,7 @@
 */
 size_t print_list(const list_t *h)
 {
-size_t i = 0;
+size_t j = 0;
 while (h)
 {
 if (h->str == NULL)
@@ -19,7 +19,7 @@ else
 {
 printf("[%d] %s\n", h->len, h->str);
 }
-i++;
+j++;
 h = h->next;
 }
 return (i);
