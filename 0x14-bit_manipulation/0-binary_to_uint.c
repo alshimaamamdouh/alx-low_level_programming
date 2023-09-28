2bd6b44b56586dd70e86cf4cc8998db83819a66d
Alshimaa Mamdouh <alshimaa.mamdouh.abdelaziz@gmail.com>
2023-09-28 15:53:27 -0700

    task0

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - Converts a binary number to an unsigned int.
+ * @b: A pointer
+ *
+ *
+ * Return: If b is NULL or contains chars not 0 or 1 - 0.
+ *         Otherwise - the converted number.
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+unsigned int k = 1;
+unsigned int i = 0;
+int c;
+unsigned int len;
+
+for (len = 0; b[len];)
+len++;
+
+for (c = len - 1; c >= 0; c--)
+{
+if (b[c] != '0' && b[c] != '1')
+return (0);
+
+if (b[c] == '1')
+{
+i += k;
+}
+k *= 2;
+}
+return (i);
+}
---
+++ 0x14-bit_manipulation/README.md
@@ -0,0 +1,1 @@
+bit_manipulation task readme file
