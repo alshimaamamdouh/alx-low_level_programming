150d62a7230d3eb286de6aca60dc719fa8ad95db
Alshimaa Mamdouh <alshimaa.mamdouh.abdelaziz@gmail.com>
2023-10-02 16:25:10 -0700

    tasks

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ * read_textfile - reads a text file and prints
+ * @filename: input filename
+ * @letters: input length
+ *
+ *
+ * Return: number of letters it could read and print, 0 
+ *
+ *
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+
+ssize_t file, read_file, write_file; 
+
+char *bufer;
+buffer = malloc(letters);
+if (buffer == NULL)
+{
+return (0);
+
+}
+if (!filename) 
+
+{ 
+
+return (0);
+}
+file = open(filename, O_RDONLY);
+if (file == -1)
+{
+free(text);
+return (0);
+}
+read_file = read(file, buffer, letters);
+write_file = write(STDOUT_FILENO, buffer, read_file);
+close(file);
+return (write_file;);
+}
---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ * create_file - creates a file.
+ * @filename: input filename
+ * @text_content: input file text
+ *
+ * Return: 1 on success, -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+int file, i, j;
+if (!filename)
+return (-1);
+
+if (text_content == NULL)
+text_content = "";
+
+for (i = 0; text_content[i] != '\0'; )
+i++;
+
+file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+if (file == -1)
+return (-1);
+
+write(file, text_content, i);
+return (1);
+}
